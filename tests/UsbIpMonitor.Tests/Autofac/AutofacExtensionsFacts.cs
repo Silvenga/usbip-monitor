ba4938c6a7f49d2a90ac581a971d49febca7fd8a
Mark Lopez <m@silvenga.com>
2023-03-22 12:44:33 -0500

    Added coverage to AutofacExtensions.

---
+++ tests/UsbIpMonitor.Tests/Autofac/AutofacExtensionsFacts.cs
@@ -0,0 +1,54 @@
+﻿using Autofac;
+using FluentAssertions;
+using UsbIpMonitor.Autofac;
+using Xunit;
+
+namespace UsbIpMonitor.Tests.Autofac
+{
+    public class AutofacExtensionsFacts
+    {
+        [Fact]
+        public void When_contrast_conventions_are_applied_then_Resolve_should_succeed_on_concrete_types()
+        {
+            var builder = new ContainerBuilder();
+
+            // Act
+            builder.ApplyConventions(typeof(AutofacExtensionsFacts).Assembly);
+
+            // Assert
+            var container = builder.Build();
+            container.Resolve<ConcreteClassFixture>().Should().NotBeNull();
+        }
+
+        [Fact]
+        public void When_contrast_conventions_are_applied_then_Resolve_should_succeed_on_default_interfaces()
+        {
+            var builder = new ContainerBuilder();
+
+            // Act
+            builder.ApplyConventions(typeof(AutofacExtensionsFacts).Assembly);
+
+            // Assert
+            var container = builder.Build();
+            container.Resolve<IDefaultInterfaceFixture>().Should().NotBeNull();
+        }
+
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable ClassNeverInstantiated.Global
+        // ReSharper disable UnusedType.Global
+        public class ConcreteClassFixture
+        {
+        }
+
+        public interface IDefaultInterfaceFixture
+        {
+        }
+
+        public class DefaultInterfaceFixture : IDefaultInterfaceFixture
+        {
+        }
+        // ReSharper restore ClassNeverInstantiated.Global
+        // ReSharper restore MemberCanBePrivate.Global
+        // ReSharper restore UnusedType.Global
+    }
+}
